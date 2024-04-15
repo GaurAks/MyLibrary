fd0a37b82700798f85538f6739da970de5e7942f
GaurAks <61326062+GaurAks@users.noreply.github.com>
2024-04-15 19:06:02 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MyLibrary",
-            targets: ["MyLibrary","FirstFramework","Toast"]),
+            targets: ["MyLibrary","FirstFramework"]),
     ],
     dependencies: [
         .package(url: "https://github.com/BastiaanJansen/toast-swift", from: "2.1.2")

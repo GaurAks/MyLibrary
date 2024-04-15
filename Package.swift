0388d6339a35b6e48b623d6c5a73f8c036988eb6
GaurAks <61326062+GaurAks@users.noreply.github.com>
2024-04-15 19:02:50 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MyLibrary",
-            targets: ["MyLibrary","FirstFramework"]),
+            targets: ["MyLibrary","FirstFramework","Toast"]),
     ],
     dependencies: [
         .package(url: "https://github.com/BastiaanJansen/toast-swift", from: "2.1.2")
@@ -21,7 +21,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyLibrary"
+            name: "MyLibrary",
+            dependencies: ["Toast"]
         ),
         .binaryTarget(
             name: "FirstFramework",

5aa8b758ec82ec207657531978d48b971eac4fdd
GaurAks <61326062+GaurAks@users.noreply.github.com>
2024-04-15 18:19:08 +0530

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MyLibrary",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MyLibrary",
+            targets: ["MyLibrary"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/BastiaanJansen/toast-swift", from: "2.1.2")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MyLibrary"
+        ),
+        .binaryTarget(
+            name: "FirstFramework",
+            path: "./Sources/FirstFramework.xcframework"
+        ),
+    ]
+)
